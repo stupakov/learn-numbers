ceea317097eeb6df7b5c5bc00841935dceb245ec
Alex Stupakov <stupakov@gmail.com>
2018-06-19 17:23:34 -0700

    add react-slick carousel

---
+++ src/components/Carousel.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import "./carousel.css";
+
+export default class Carousel extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true
+    };
+    return (
+      <div>
+        <Slider {...settings} className='control-slider'>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+        </Slider>
+      </div>
+    );
+  }
+}
