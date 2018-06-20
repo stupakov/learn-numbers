b23256d9daafc99fd3af52e90d46f615fbd028a7
Alex Stupakov <stupakov@gmail.com>
2018-06-19 17:55:23 -0700

    set up two linked sliders carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -7,29 +7,71 @@ import "slick-carousel/slick/slick-theme.css";
 import "./carousel.css";
 
 export default class Carousel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nav1: null,
+      nav2: null
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      nav1: this.slider1,
+      nav2: this.slider2
+    });
+  }
+
   render() {
-    const settings = {
+    const viewSliderSettings = {
       dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false
+    };
+    const controlSliderSettings = {
+      dots: false,
       infinite: false,
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
       arrows: true
     };
+
     return (
       <div>
-        <Slider {...settings} className='control-slider'>
+        <Slider
+          asNavFor={this.state.nav2}
+          ref={slider => (this.slider1 = slider)}
+          {...viewSliderSettings}
+          className='view-slider'
+        >
           <div>
-            <h3>1</h3>
+            <h3>0-9</h3>
           </div>
           <div>
-            <h3>2</h3>
+            <h3>10-19</h3>
           </div>
           <div>
-            <h3>3</h3>
+            <h3>10, 100, 1k, 1M</h3>
+          </div>
+        </Slider>
+        <Slider
+          asNavFor={this.state.nav1}
+          ref={slider => (this.slider2 = slider)}
+          {...controlSliderSettings}
+          className='control-slider'
+        >
+          <div>
+            <h3>0-9</h3>
           </div>
           <div>
-            <h3>4</h3>
+            <h3>10-19</h3>
+          </div>
+          <div>
+            <h3>10, 100, 1k, 1M</h3>
           </div>
         </Slider>
       </div>
