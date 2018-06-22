ad9760e334469e40a3c91fa7042858f76b93cb22
Alex Stupakov <stupakov@gmail.com>
2018-06-22 15:40:47 -0700

    add carousel to reference page

--- src/components/ReferencePage.js
+++ src/components/ReferencePage.js
@@ -1,9 +1,24 @@
 import React from "react";
+import Carousel from './Carousel';
 
 const ReferencePage = () => (
-  <div>
-    Reference
-  </div>
+  <Carousel>
+    <div className='slide' label='0-9'>
+      0-9
+    </div>
+    <div className='slide' label='10-19'>
+      10-19
+    </div>
+    <div className='slide' label='10-100'>
+      10-100
+    </div>
+    <div className='slide' label='100-1000'>
+      100-10000
+    </div>
+    <div className='slide' label='10, 100, 1k, 1M'>
+      10, 100, 1k, 1M
+    </div>
+  </Carousel>
 );
 
 export default ReferencePage;
