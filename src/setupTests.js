9969cda1e20d0ab6d0242bc978fa193c832ecbf2
Alex Stupakov <stupakov@gmail.com>
2018-07-27 14:56:59 -0700

    add polyfill to test mode to reduce test errors

---
+++ src/setupTests.js
@@ -0,0 +1,11 @@
+// create-react-app loads this file by default in test mode
+
+window.matchMedia =
+  window.matchMedia ||
+  function() {
+    return {
+      matches: false,
+      addListener: function() {},
+      removeListener: function() {}
+    };
+  };
